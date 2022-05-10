e85a4d9d848d4f33ee38cb95295522e393d35b25
lutinskae <99957070+lutinskae@users.noreply.github.com>
2022-05-10 22:20:21 +0300

    Update kursa_darbs.py

--- kursa_darbs.py
+++ kursa_darbs.py
@@ -1,4 +1,4 @@
-# from tkinter import Y
+from tkinter import Y
 import turtle
 import math
 # li = ["Kursa darbs!", "Tēma", "--"]
@@ -8,8 +8,17 @@ screen = turtle.Screen()
 # background color
 screen.bgcolor("lightblue")
 # turtle object
-
 def SaulesZiimeejums(): #funkcija, lai izveidotu sauli
+    y = turtle.Turtle()
+    def drawFourRays(t, length, radius): #Funkcija funkcijā
+        for i in range(4):
+            t.penup()
+            t.forward(radius)
+            t.pendown()
+            t.forward(length)
+            t.penup()
+            t.backward(length + radius)
+            t.left(90)
     y.penup()
     y.goto(95, 120)
     y.fillcolor("yellow")
@@ -25,121 +34,90 @@ def SaulesZiimeejums(): #funkcija, lai izveidotu sauli
     y.right(45)
     drawFourRays(y, 95, 64)
     y.left(45)
-    k = turtle.Turtle()
+    
     #color and speed
     # of turtle
-
 def MaajasZiimeejums(): #funkcija, lai izveidotu māju
-    k = turtle.Turtle()
-    k.color("black")
-    k.shape("turtle")
-    k.speed(1)
-    # veido majas pirmo stavu
-    k.fillcolor('cyan')
-    k.begin_fill()
-    k.right(90)
-    k.forward(250)
-    k.left(90)
-    k.forward(400)
-    k.left(90)
-    k.forward(250)
-    k.left(90)
-    k.forward(400)
-    k.right(90)
-    k.end_fill()
-    # veido majas jumtu
-    k.fillcolor('brown')
-    k.begin_fill()
-    k.right(45)
-    k.forward(200)
-    k.right(90)
-    k.forward(200)
-    k.left(180)
-    k.forward(200)
-    k.right(135)
-    k.forward(259)
-    k.right(90)
-    k.forward(142)
-    k.end_fill()
-    # for door and
+    z = turtle.Turtle()
+    z.penup()
+    z.goto(145, 170)
+    z.color("black")
+    z.shape("turtle")
+    z.speed(2)
+    # base of the house
+    z.fillcolor('black')
+    z.begin_fill()
+    z.right(90)
+    z.forward(250)
+    z.left(90)
+    z.forward(400)
+    z.left(90)
+    z.forward(250)
+    z.left(90)
+    z.forward(400)
+    z.right(90)
+    z.end_fill()
+    # for top of
+    # the house
+    z.fillcolor('red')
+    z.begin_fill()
+    z.right(45)
+    z.forward(282)
+    z.right(90)
+    z.forward(282)
+    z.right(45)
+    z.end_fill()
     # windows
-    y.right(90)
-    y.forward(400)
-    y.left(90)
-    y.forward(50)
-    y.left(90)
-    y.forward(150)
-    y.right(90)
-    y.forward(200)
-    y.right(180)
-    y.forward(200)
-    y.right(90)
-    y.forward(200)
-    y.right(90)
-    y.forward(150)
-    y.right(90)
-    y.forward(200)
-    y.right(90)
-    y.forward(150)
-    y.right(90)
-    y.forward(100)
-    y.right(90)
-    y.forward(150)
-    y.right(90)
-    y.forward(100)
-    y.right(90)
-    y.forward(75)
-    y.right(90)
-    y.forward(200)
-    y.right(180)
-    y.forward(200)
-    y.right(90)
-    y.forward(75)
-    y.left(90)
-    y.forward(15)
-    y.left(90)
-    y.forward(200)
-    y.right(90)
-    y.forward(15)
-    y.right(90)
-    y.forward(75)
-
-y = turtle.Turtle()
-
-izveeleetie = []
-
-while True:
-    try:
-        opcija = int(input("Izvēlieties opcijas\n1 Saule\n2 Māja\n3 Koks\n"))
-        if opcija == 1:
-            izveeleetie.append("Sauli")
-        if opcija == 2:
-            izveeleetie.append("Māju")
-        if opcija == 3:
-            izveeleetie.append("Koku")  
-
-    except: print("Ievadiet skaitli!")
-
-
-    # definee funkciju lai zimeetu sauliti
-    def drawFourRays(t, length, radius):
-        for i in range(4):
-            t.penup()
-            t.forward(radius)
-            t.pendown()
-            t.forward(length)
-            t.penup()
-            t.backward(length + radius)
-            t.left(90)
-    # uzzimee apli, prieks saules
-    if opcija == 1: #Zīmēt sauli
-        SaulesZiimeejums()
-    
-    if opcija == 2:
-        MaajasZiimeejums()
-    
-    #peec sis rindinas saksies koka zimesanas dala
-    # lai uzzimetu celmu
+    z.right(90)
+    z.forward(400)
+    z.left(90)
+    z.forward(50)
+    z.left(90)
+    z.forward(150)
+    z.right(90)
+    z.forward(200)
+    z.right(180)
+    z.forward(200)
+    z.fillcolor('white')
+    z.begin_fill()
+    z.right(90)
+    z.forward(200)
+    z.right(90)
+    z.forward(150)
+    z.right(90)
+    z.forward(200)
+    z.right(90)
+    z.forward(150)
+    z.right(90)
+    z.forward(100)
+    z.end_fill()
+    z.right(90)
+    z.forward(150)
+    z.right(90)
+    z.forward(100)
+    z.right(90)
+    z.forward(75)
+    z.right(90)
+    z.forward(200)
+    z.right(180)
+    z.forward(200)
+    # for door
+    z.right(90)
+    z.forward(75)
+    z.left(90)
+    z.forward(15)
+    z.left(90)
+    z.forward(200)
+    z.right(90)
+    z.forward(15)
+    z.right(90)
+    z.forward(75)
+    z.fillcolor('brown')
+    z.begin_fill()
+    z.circle(10)
+    z.end_fill()
+    z.forward(120)
+def KokaZiimeejums():
     def drawRectangle(t, width, height, color):
         t.fillcolor(color)
         t.begin_fill()
@@ -164,40 +142,48 @@ while True:
         t.left(135)
         t.end_fill()
         # izveido turtle
-    if opcija == 3:
-        tip = turtle.Turtle()
-        tip.color ("black")
-        tip.shape ("turtle")
-        tip.speed (2)
-        # Tree base
-        tip.penup()
-        tip.goto(-100, -130)
-        tip.pendown()
-        drawRectangle(tip, 20, 40, "brown")
-        # Tree top
-        tip.penup()
-        tip.goto(-135, -90)
-        tip.pendown()
-        drawTriangle(tip, 90, "lightgreen")
-        tip.penup()
-        tip.goto(-130, -45)
-        tip.pendown()
-        drawTriangle(tip, 80, "lightgreen")
-        tip.penup()
-        tip.goto(-125, -5)
-        tip.pendown()
-        drawTriangle(tip, 70, "lightgreen")
-        tip.penup()
-    
-    
+    tip = turtle.Turtle()
+    tip.color ("black")
+    tip.shape ("turtle")
+    tip.speed (2)
+    # Tree base
+    tip.penup()
+    tip.goto(-100, -130)
+    tip.pendown()
+    drawRectangle(tip, 20, 40, "brown")
+    # Tree top
+    tip.penup()
+    tip.goto(-135, -90)
+    tip.pendown()
+    drawTriangle(tip, 90, "lightgreen")
+    tip.penup()
+    tip.goto(-130, -45)
+    tip.pendown()
+    drawTriangle(tip, 80, "lightgreen")
+    tip.penup()
+    tip.goto(-125, -5)
+    tip.pendown()
+    drawTriangle(tip, 70, "lightgreen")
+    tip.penup()
+
+izveeleetie = []
+while True:
+    try:
+        opcija = int(input("Izvēlieties opcijas\n1 Saule\n2 Māja\n3 Koks\n"))
+        if opcija == 1:
+            izveeleetie.append("Sauli")
+            SaulesZiimeejums()
+        if opcija == 2:
+            izveeleetie.append("Māju")
+            MaajasZiimeejums()
+        if opcija == 3:
+            izveeleetie.append("Koku")
+            KokaZiimeejums()
+    except: print("Ievadiet skaitli!")
     izvele = str(input("Vai turpināt zīmēšanu?\nY Jā \nN Nē"))
     if izvele == "N" or izvele == "n": break
-    
-
-fails = open("Rezultati.txt", "w")
+fails = open("Rezultati.txt", "w", encoding='utf-8')
 fails.write("Lietotajs izvelejas: ")
-
 for x in range(len(izveeleetie)):
     fails.write(f"{izveeleetie[x]}, ")
-
 fails.close()
